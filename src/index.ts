b52366f9b8f330b5f6522bacf6d74f7ddf647dd4
João Monte <joaovictormrtavares@gmail.com>
2020-11-21 15:24:28 -0300

    feat: configured husky

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import * as express from 'express';
+import * as express from "express";
 
 const app = express();
 const port = 9898;
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
